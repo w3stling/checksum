75d76aa5b9edf14cb5d9807c34c3df3691f5abbf
w3stling <peter.westling@gmail.com>
2019-03-21 06:56:51 +0100

    Test added

---
+++ src/test/java/com/apptastic/checksum/StockholmSlTest.java
@@ -0,0 +1,90 @@
+package com.apptastic.checksum;
+
+import org.junit.Test;
+
+import java.net.URI;
+import java.security.MessageDigest;
+
+import static org.junit.Assert.assertEquals;
+
+public class StockholmSlTest {
+
+    @Test
+    public void allRailServicesMap() throws Exception {
+        String checksum = Checksum.calculate(new URI("https://sl.se/ficktid/karta/vinter/SL_Sp%C3%A5rtrafik.pdf"), MessageDigest.getInstance("MD5"));
+        assertEquals("f57d91f9b1592d33b80360abee42e521", checksum);
+    }
+
+    @Test
+    public void tramPendeltagMap() throws Exception {
+        String checksum = Checksum.calculate(new URI("https://sl.se/ficktid/karta/vinter/Ptag.pdf"), MessageDigest.getInstance("MD5"));
+        assertEquals("4aa6a42e28bc275bc21bb8013669b391", checksum);
+    }
+
+    @Test
+    public void metroMap() throws Exception {
+        String checksum = Checksum.calculate(new URI("https://sl.se/ficktid/karta/vinter/Tub.pdf"), MessageDigest.getInstance("MD5"));
+        assertEquals("53520bd45960c1aab1738407ac4df750", checksum);
+    }
+
+    @Test
+    public void tramLidingoMap() throws Exception {
+        String checksum = Checksum.calculate(new URI("https://sl.se/ficktid/karta/vinter/LB.pdf"), MessageDigest.getInstance("MD5"));
+        assertEquals("698a6257fb1a142f6c5b544dec810453", checksum);
+    }
+
+    @Test
+    public void tramNockebybananMap() throws Exception {
+        String checksum = Checksum.calculate(new URI("https://sl.se/ficktid/karta/vinter/Nb.pdf"), MessageDigest.getInstance("MD5"));
+        assertEquals("58090a6915fa6c7092cee120c6a6a9a5", checksum);
+    }
+
+    @Test
+    public void tramNockebybananTvarbananMap() throws Exception {
+        String checksum = Checksum.calculate(new URI("https://sl.se/ficktid/karta/vinter/NbTB.pdf"), MessageDigest.getInstance("MD5"));
+        assertEquals("2587e8151ba63d3a9848feed6b3f7bbc", checksum);
+    }
+
+    @Test
+    public void tramRoslagsbananMap() throws Exception {
+        String checksum = Checksum.calculate(new URI("https://sl.se/ficktid/karta/vinter/Rb.pdf"), MessageDigest.getInstance("MD5"));
+        assertEquals("4ea95b723272186dfe34c169cd0f3fe0", checksum);
+    }
+
+    @Test
+    public void tramSaltsjobananMap() throws Exception {
+        String checksum = Checksum.calculate(new URI("https://sl.se/ficktid/karta/vinter/SB.pdf"), MessageDigest.getInstance("MD5"));
+        assertEquals("1af39feee5cda715150b789c31dc3ce1", checksum);
+    }
+
+    @Test
+    public void tramCityMap() throws Exception {
+        String checksum = Checksum.calculate(new URI("https://sl.se/ficktid/karta/vinter/Sparvag_city.pdf"), MessageDigest.getInstance("MD5"));
+        assertEquals("1f0bb1bce244cc47bb946258350e5857", checksum);
+    }
+
+    @Test
+    public void tramTvarbananMap() throws Exception {
+        String checksum = Checksum.calculate(new URI("https://sl.se/ficktid/karta/vinter/tb.pdf"), MessageDigest.getInstance("MD5"));
+        assertEquals("a16f8c23269b519761ef85df8b57ee78", checksum);
+    }
+
+    @Test
+    public void commuterFerryLinesLine80_82Map() throws Exception {
+        String checksum = Checksum.calculate(new URI("https://sl.se/globalassets/terminalkarta/linje8082.pdf"), MessageDigest.getInstance("MD5"));
+        assertEquals("c0b0c43901313e5c0c53f344fa3590a8", checksum);
+    }
+
+    @Test
+    public void commuterFerryLinesLine82Map() throws Exception {
+        String checksum = Checksum.calculate(new URI("https://sl.se/globalassets/terminalkarta/linje82.pdf"), MessageDigest.getInstance("MD5"));
+        assertEquals("de4a07b95f69546cc1a3ba800e9e0446", checksum);
+    }
+
+    @Test
+    public void commuterFerryLinesLine89Map() throws Exception {
+        String checksum = Checksum.calculate(new URI("https://sl.se/ficktid/karta/vinter/linje89.pdf"), MessageDigest.getInstance("MD5"));
+        assertEquals("f9c3bf68d31e1fc027d81cdb88047915", checksum);
+    }
+
+}
