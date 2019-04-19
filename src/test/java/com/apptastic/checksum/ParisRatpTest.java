e904f3ad3ce7843b978f7f2f47d4f872e242c899
w3stling <peter.westling@gmail.com>
2019-04-19 15:55:00 +0200

    Updated test case

--- src/test/java/com/apptastic/checksum/ParisRatpTest.java
+++ src/test/java/com/apptastic/checksum/ParisRatpTest.java
@@ -12,7 +12,7 @@ public class ParisRatpTest {
     @Test
     public void metroMap() throws Exception{
         String checksum = Checksum.calculate(new URI("https://www.ratp.fr/sites/default/files/plans-lignes/Plans-essentiels/Plan-Metro.1542892915.pdf"), MessageDigest.getInstance("MD5"));
-        assertEquals("400601a784ba0aaf3622681877b82307", checksum);
+        assertEquals("d2725d02131362461c32c68d336b1407", checksum);
     }
 
     @Test
@@ -24,7 +24,7 @@ public class ParisRatpTest {
     @Test
     public void busMap() throws Exception{
         String checksum = Checksum.calculate(new URI("https://www.ratp.fr/sites/default/files/plans-lignes/Plans-essentiels/Plan-des-Bus.1542892928.pdf"), MessageDigest.getInstance("MD5"));
-        assertEquals("8d4794baad35d335a6ab041b3017697e", checksum);
+        assertEquals("dac2e410865e9a6a0317b67f22aaae27", checksum);
     }
 
     @Test
