6d9e6123398721ce343cc741aac2690a798edf90
w3stling <peter.westling@gmail.com>
2019-04-23 19:10:31 +0200

    Test added

---
+++ src/test/java/com/apptastic/checksum/MicCodeTest.java
@@ -0,0 +1,17 @@
+package com.apptastic.checksum;
+
+import org.junit.Test;
+
+import java.net.URI;
+import java.security.MessageDigest;
+
+import static org.junit.Assert.assertEquals;
+
+public class MicCodeTest {
+
+    @Test
+    public void MicCodeTest() throws Exception {
+        String checksum = Checksum.calculate(new URI("https://www.iso20022.org/sites/default/files/ISO10383_MIC/ISO10383_MIC.csv"), MessageDigest.getInstance("MD5"));
+        assertEquals("62617e132a580a54e130b195cb69fc92", checksum);
+    }
+}
