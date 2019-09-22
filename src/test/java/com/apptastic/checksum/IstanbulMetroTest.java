ed439fa9fa121a29713037d7e5855a130b4ecb77
w3stling <peter.westling@gmail.com>
2019-09-22 16:57:27 +0200

    Updated test case

--- src/test/java/com/apptastic/checksum/IstanbulMetroTest.java
+++ src/test/java/com/apptastic/checksum/IstanbulMetroTest.java
@@ -1,6 +1,5 @@
 package com.apptastic.checksum;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.net.URI;
@@ -12,19 +11,19 @@ public class IstanbulMetroTest {
 
     @Test
     public void railSystemsMap() throws Exception {
-        String checksum = Checksum.calculate(new URI("http://www.metro.istanbul/Content/assets/uploaded/%C4%B0stanbul%20Rayl%C4%B1%20Sistemler%20Haritas%C4%B1.pdf"), MessageDigest.getInstance("MD5"));
+        String checksum = Checksum.calculate(new URI("https://www.metro.istanbul/Content/assets/uploaded/%C4%B0stanbul%20Rayl%C4%B1%20Sistemler%20Haritas%C4%B1.pdf"), MessageDigest.getInstance("MD5"));
         assertEquals("f51acd0143e4bb533da666d9cd8c7c96", checksum);
     }
 
     @Test
     public void tramMap() throws Exception {
-        String checksum = Checksum.calculate(new URI("http://www.metro.istanbul/Content/assets/uploaded/%C4%B0stanbul%20Tramvay%20Hatlar%C4%B1%20Haritas%C4%B1.pdf"), MessageDigest.getInstance("MD5"));
+        String checksum = Checksum.calculate(new URI("https://www.metro.istanbul/Content/assets/uploaded/%C4%B0stanbul%20Tramvay%20Hatlar%C4%B1%20Haritas%C4%B1.pdf"), MessageDigest.getInstance("MD5"));
         assertEquals("349e29c88b4ab4f22f703263167c5490", checksum);
     }
 
     @Test
     public void metroMap() throws Exception {
-        String checksum = Checksum.calculate(new URI("http://www.metro.istanbul/Content/assets/uploaded/%C4%B0stanbul%20Metro%20Hatlar%C4%B1%20Haritas%C4%B1.pdf"), MessageDigest.getInstance("MD5"));
+        String checksum = Checksum.calculate(new URI("https://www.metro.istanbul/Content/assets/uploaded/%C4%B0stanbul%20Metro%20Hatlar%C4%B1%20Haritas%C4%B1.pdf"), MessageDigest.getInstance("MD5"));
         assertEquals("3f703256e54ccd3094ebb6fac7da28f6", checksum);
     }
 
