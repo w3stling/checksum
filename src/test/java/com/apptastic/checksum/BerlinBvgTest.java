7770509325de45fe0fc8f3dfa9cac065c87d14aa
w3stling <peter.westling@gmail.com>
2020-04-29 00:37:13 +0200

    Updated test case

--- src/test/java/com/apptastic/checksum/BerlinBvgTest.java
+++ src/test/java/com/apptastic/checksum/BerlinBvgTest.java
@@ -12,19 +12,19 @@ public class BerlinBvgTest {
     @Test
     public void undergroundBerlinABC() throws Exception {
         String checksum = Checksum.calculate(new URI("https://www.bvg.de/de/index.php?section=downloads&cmd=58&download=399"), MessageDigest.getInstance("MD5"));
-        assertEquals("788f7f9573cd69c5be271bea65b93f69", checksum);
+        assertEquals("8374e7e667b08d6a6cd6bbc1088f1643", checksum);
     }
 
     @Test
     public void undergroundBerlinCityCenter() throws Exception {
         String checksum = Checksum.calculate(new URI("https://www.bvg.de/de/index.php?section=downloads&cmd=58&download=400"), MessageDigest.getInstance("MD5"));
-        assertEquals("f70f9c4ae1289a7de0fd22f5e06306be", checksum);
+        assertEquals("7ec6a691ddeab9045bb6e5c90cf67f83", checksum);
     }
 
     @Test
     public void tramMap() throws Exception {
         String checksum = Checksum.calculate(new URI("https://www.bvg.de/de/index.php?section=downloads&cmd=58&download=401"), MessageDigest.getInstance("MD5"));
-        assertEquals("be96aee599ab24e328e9817c9486b7d8", checksum);
+        assertEquals("9cef908eb23c1e1fa649aea68ab43326", checksum);
     }
 
     @Test
