e9f65278f6aa749510b85e7a0be8921cb9bded2a
w3stling <peter.westling@gmail.com>
2020-05-02 06:59:14 +0200

    Updated test case

--- src/test/java/com/apptastic/checksum/MicCodeTest.java
+++ src/test/java/com/apptastic/checksum/MicCodeTest.java
@@ -4,24 +4,14 @@ import org.junit.Test;
 
 import java.net.URI;
 import java.security.MessageDigest;
-import java.time.YearMonth;
 
 import static org.junit.Assert.assertEquals;
 
 public class MicCodeTest {
-    private static final String URL_FORMAT = "https://www.iso20022.org/sites/default/files/%s/ISO10383_MIC.csv";
 
     @Test
     public void MicCodeTest() throws Exception {
-        String expectedValue = "098007c20a1abd8104f435597fdc190e";
-        YearMonth yearMonth = YearMonth.now();
-
-        try {
-            String checksum = Checksum.calculate(new URI(String.format(URL_FORMAT, yearMonth)), MessageDigest.getInstance("MD5"));
-            assertEquals(expectedValue, checksum);
-        } catch (Exception e) {
-            String checksum = Checksum.calculate(new URI(String.format(URL_FORMAT, yearMonth.minusMonths(1))), MessageDigest.getInstance("MD5"));
-            assertEquals(expectedValue, checksum);
-        }
+        String checksum = Checksum.calculate(new URI("https://www.iso20022.org/sites/default/files/ISO10383_MIC/ISO10383_MIC.csv"), MessageDigest.getInstance("MD5"));
+        assertEquals("6c1b0dfb80ec8b1e94c4fe141b973bdc", checksum);
     }
 }
