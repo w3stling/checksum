e4ded9f68b5befc50dfc8626eb73b2e92e19c02c
w3stling <peter.westling@gmail.com>
2020-09-01 07:13:33 +0200

    Updated test case

--- src/test/java/com/apptastic/checksum/BerlinBvgTest.java
+++ src/test/java/com/apptastic/checksum/BerlinBvgTest.java
@@ -36,6 +36,6 @@ public class BerlinBvgTest {
     @Test
     public void nightBusMap() throws Exception {
         String checksum = Checksum.calculate(new URI("https://www.bvg.de/de/index.php?section=downloads&cmd=58&download=398"), MessageDigest.getInstance("MD5"));
-        assertEquals("5b4e6f09bac4f8ffccf0a5b06bd6535b", checksum);
+        assertEquals("9b909459ddfcf947433003305e847ed6", checksum);
     }
 }
