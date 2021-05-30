d557827471de48d265af69f1ebb1f6600a53da63
w3stling <peter.westling@gmail.com>
2021-05-30 10:01:24 +0200

    Updated test case

--- src/test/java/com/apptastic/checksum/IstanbulMetroTest.java
+++ src/test/java/com/apptastic/checksum/IstanbulMetroTest.java
@@ -12,19 +12,19 @@ public class IstanbulMetroTest {
     @Test
     public void railSystemsMap() throws Exception {
         String checksum = Checksum.calculate(new URI("https://www.metro.istanbul/Content/assets/uploaded/%C4%B0stanbul%20Rayl%C4%B1%20Sistemler%20Haritas%C4%B1.pdf"), MessageDigest.getInstance("MD5"));
-        assertEquals("0851c156de1f0ff047b75296e3361859", checksum);
+        assertEquals("ae2e03d7fe4ad4fff239f4573ebe83e6", checksum);
     }
 
     @Test
     public void tramMap() throws Exception {
         String checksum = Checksum.calculate(new URI("https://www.metro.istanbul/Content/assets/uploaded/%C4%B0stanbul%20Tramvay%20Hatlar%C4%B1%20Haritas%C4%B1.pdf"), MessageDigest.getInstance("MD5"));
-        assertEquals("84d8e93cb3627099dae927d5712047d9", checksum);
+        assertEquals("2f0fbc36cb586a5aa68f7e66dd1269ae", checksum);
     }
 
     @Test
     public void metroMap() throws Exception {
         String checksum = Checksum.calculate(new URI("https://www.metro.istanbul/Content/assets/uploaded/%C4%B0stanbul%20Metro%20Hatlar%C4%B1%20Haritas%C4%B1.pdf"), MessageDigest.getInstance("MD5"));
-        assertEquals("46d16e26fa31f4ad5ed0cd4e57eea4eb", checksum);
+        assertEquals("9d6f834875b3d4c577882373e2864540", checksum);
     }
 
     @Test
