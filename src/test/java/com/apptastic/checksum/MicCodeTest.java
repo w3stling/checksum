d4dc433032c11a903cfbb9f19198f2ef1a0cf936
w3stling <peter.westling@gmail.com>
2021-06-15 08:24:20 +0200

    Updated test case

--- src/test/java/com/apptastic/checksum/MicCodeTest.java
+++ src/test/java/com/apptastic/checksum/MicCodeTest.java
@@ -12,6 +12,6 @@ public class MicCodeTest {
     @Test
     public void MicCodeTest() throws Exception {
         String checksum = Checksum.calculate(new URI("https://www.iso20022.org/sites/default/files/ISO10383_MIC/ISO10383_MIC.csv"), MessageDigest.getInstance("MD5"));
-        assertEquals("4f459952204544ff6699a66aa22703ad", checksum);
+        assertEquals("9a364105c5c7acf599bb1837e2aeb7f9", checksum);
     }
 }
