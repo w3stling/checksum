694d5f775d6165e6f29880bffe7ac7192d0bc157
w3stling <peter.westling@gmail.com>
2022-02-24 23:03:51 +0100

    Updated test case

--- src/test/java/com/apptasticsoftware/checksum/BarcelonaTmbTest.java
+++ src/test/java/com/apptasticsoftware/checksum/BarcelonaTmbTest.java
@@ -12,7 +12,7 @@ class BarcelonaTmbTest {
     @Test
     void metroMap() throws Exception {
         String checksum = Checksum.calculate(new URI("https://www.tmb.cat/documents/20182/96078/Pl%C3%A0nol+xarxa+de+Metro/f5578f40-5a66-46ba-907c-cf31e0d368c0"), MessageDigest.getInstance("MD5"));
-        assertEquals("b35c3373feeaa399f26cc1ec840cc86c", checksum);
+        assertEquals("065ee6455992c4e84221511dd5f0a493", checksum);
     }
 
     @Test
--- src/test/java/com/apptasticsoftware/checksum/DelhiMetroTest.java
+++ src/test/java/com/apptasticsoftware/checksum/DelhiMetroTest.java
@@ -13,19 +13,19 @@ class DelhiMetroTest {
     @Test
     void metroMap() throws Exception {
         String checksum = Checksum.calculate(new URI("http://www.delhimetrorail.com/images/Map-Bilingual-04102019.jpg"), MessageDigest.getInstance("MD5"));
-        assertEquals("df8c25d8cc176881c849f751bfceb8d6", checksum);
+        assertEquals("4d31bb8b75c20e028b6c1cc6a9a40612", checksum);
     }
 
     @Test
     void rapidMetroMap() throws Exception {
         String checksum = Checksum.calculate(new URI("http://www.delhimetrorail.com/images/RapidMetroRouteMap.jpg"), MessageDigest.getInstance("MD5"));
-        assertEquals("4e84ea188d3857f592544beeeae1eba6", checksum);
+        assertEquals("4d31bb8b75c20e028b6c1cc6a9a40612", checksum);
     }
 
     @Test
     void airportExpressLine() throws Exception {
         String checksum = Checksum.calculate(new URI("http://www.delhimetrorail.com/images/airportlineimg.jpg"), MessageDigest.getInstance("MD5"));
-        assertEquals("527d9ea1037cd6b964cb8e09c87e7d2c", checksum);
+        assertEquals("4d31bb8b75c20e028b6c1cc6a9a40612", checksum);
     }
 
     @Disabled("Slow")
