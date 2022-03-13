f469e74640de02abab36525ab30e47b6e7134934
w3stling <peter.westling@gmail.com>
2022-03-13 17:53:35 +0100

    Updated test case

--- src/test/java/com/apptasticsoftware/checksum/BarcelonaTmbTest.java
+++ src/test/java/com/apptasticsoftware/checksum/BarcelonaTmbTest.java
@@ -18,7 +18,7 @@ class BarcelonaTmbTest {
     @Test
     void busMap() throws Exception {
         String checksum = Checksum.calculate(new URI("https://www.tmb.cat/documents/20182/96078/Pl%C3%A0nol+xarxa+bus.pdf/9b5bfa95-6117-4117-9843-fb3e3fde5447"), MessageDigest.getInstance("MD5"));
-        assertEquals("9f9c0687e8bce35cdd63cdfcd45b05a0", checksum);
+        assertEquals("349f1bad7ad286efbac9030c59c48ec0", checksum);
     }
 
     @Test
--- src/test/java/com/apptasticsoftware/checksum/MadridMetroTest.java
+++ src/test/java/com/apptasticsoftware/checksum/MadridMetroTest.java
@@ -12,7 +12,7 @@ class MadridMetroTest {
     @Test
     void metroAndLightRailNetworkMap() throws Exception {
         String checksum = Checksum.calculate(new URI("https://www.metromadrid.es/sites/default/files/documentos/Viaja%20en%20Metro/Planos/Planoesquematico.pdf"), MessageDigest.getInstance("MD5"));
-        assertEquals("8b3c045d1dbe95f6c654a0c92e287b3e", checksum);
+        assertEquals("7686fdd6d0f34ba576cc60ea5f1d6327", checksum);
     }
 
     @Test
