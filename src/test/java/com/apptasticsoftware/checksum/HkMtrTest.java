855e5dcf364d3e77404e4fa4ecb9f21a7dfce099
w3stling <peter.westling@gmail.com>
2022-07-24 20:34:10 +0200

    Updated test case

--- src/test/java/com/apptasticsoftware/checksum/HkMtrTest.java
+++ src/test/java/com/apptasticsoftware/checksum/HkMtrTest.java
@@ -12,33 +12,31 @@ class HkMtrTest {
 
     @Test
     void systemMap() throws Exception {
-        String checksum = Checksum.calculate(new URI("http://www.mtr.com.hk/archive/en/services/routemap.pdf"), MessageDigest.getInstance("MD5"));
+        String checksum = Checksum.calculate(new URI("https://www.mtr.com.hk/archive/en/services/routemap.pdf"), MessageDigest.getInstance("MD5"));
         assertEquals("28c90fd21a9dfb6b3d14c875acbd1888", checksum);
     }
 
     @Test
     void lightRailRouteMap() throws Exception {
-        String checksum = Checksum.calculate(new URI("http://www.mtr.com.hk/archive/en/services/LR_routemap.pdf"), MessageDigest.getInstance("MD5"));
+        String checksum = Checksum.calculate(new URI("https://www.mtr.com.hk/archive/en/services/LR_routemap.pdf"), MessageDigest.getInstance("MD5"));
         assertEquals("a5caede846e5dd7a6624424ee87ba8dc", checksum);
     }
 
-    @Disabled("Investigating")
     @Test
     void westboundTramwayMap() throws Exception {
-        String checksum = Checksum.calculate(new URI("http://hktramways.com/images/googleMap/HK-tram-route-map-WB.pdf"), MessageDigest.getInstance("MD5"));
-        assertEquals("1d960b16b7c83a9c0af85a3efb777768", checksum);
+        String checksum = Checksum.calculate(new URI("https://hktramways.com/images/googleMap/routeMapWB.jpg"), MessageDigest.getInstance("MD5"));
+        assertEquals("da6a7e630e40e0d378d120da2d32b77d", checksum);
     }
 
-    @Disabled("Investigating")
     @Test
     void eastboundTramwayMap() throws Exception {
-        String checksum = Checksum.calculate(new URI("http://hktramways.com/images/googleMap/HK-tram-route-map-EB.pdf"), MessageDigest.getInstance("MD5"));
-        assertEquals("8db70ab801e9254e6f53ae3a769d69ad", checksum);
+        String checksum = Checksum.calculate(new URI("https://hktramways.com/images/googleMap/routeMapEB.jpg"), MessageDigest.getInstance("MD5"));
+        assertEquals("411897bff81ff59cbc8c9ceb31d8046c", checksum);
     }
 
     @Test
     void starferryRouteMap() throws Exception {
-        String checksum = Checksum.calculate(new URI("http://www.starferry.com.hk/sites/default/files/routemap.jpg"), MessageDigest.getInstance("MD5"));
+        String checksum = Checksum.calculate(new URI("https://www.starferry.com.hk/sites/default/files/routemap.jpg"), MessageDigest.getInstance("MD5"));
         assertEquals("98e9d4e062cc5d25e9b29d4c3bf3ed23", checksum);
     }
 
