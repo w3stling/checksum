c53bd5475650565ede1bac2cb1e79c9561e99455
w3stling <peter.westling@gmail.com>
2026-02-14 10:37:43 +0100

    Set http headers

--- src/main/java/com/apptasticsoftware/checksum/Checksum.java
+++ src/main/java/com/apptasticsoftware/checksum/Checksum.java
@@ -48,8 +48,10 @@ public final class Checksum {
     private static final String[] HTTP_USER_AGENTS = {
             // Chrome
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
             // Firefox
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:147.0) Gecko/20100101 Firefox/147.0",
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:147.0) Gecko/20100101 Firefox/147.0",
             // Safari
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_7_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.3 Safari/605.1.15"
     };
